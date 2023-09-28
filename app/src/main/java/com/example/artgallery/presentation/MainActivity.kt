eb58e5d4f2834350866481ffdfffae19517d961c
Коновалова Лера <73699428+lerapenguin11@users.noreply.github.com>
2023-09-28 19:20:42 +0500

    screen layout welcome

--- app/src/main/java/com/example/artgallery/presentation/MainActivity.kt
+++ app/src/main/java/com/example/artgallery/presentation/MainActivity.kt
@@ -2,8 +2,12 @@ package com.example.artgallery.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.artgallery.R
 import com.example.artgallery.databinding.ActivityMainBinding
+import com.example.artgallery.presentation.onboarding.FirstOnBoardingFragment
 import com.example.artgallery.utilits.APP_ACTIVITY
+import com.example.artgallery.utilits.replaceFragmentMain
 import com.example.artgallery.utilits.setStatusBarGradiantMain
 
 class MainActivity : AppCompatActivity() {
@@ -15,5 +19,27 @@ class MainActivity : AppCompatActivity() {
         APP_ACTIVITY = this
         setContentView(binding.root)
         setStatusBarGradiantMain(this)
+
+        replaceFragmentMain(FirstOnBoardingFragment())
+
+       /* binding.bottomNavigationView.setOnItemSelectedListener {
+
+            when(it.itemId){
+
+                R.id.home -> replaceFragmentMain(HomeFragment())
+                R.id.save -> replaceFragmentMain(SaveFragment())
+
+                else -> hideBottomNavigationView()
+            }
+            true
+        }*/
+    }
+
+    fun hideBottomNavigationView() {
+        binding.bottomNavigationView.visibility = View.GONE
+    }
+
+    fun showBottomNavigationView() {
+        binding.bottomNavigationView.visibility = View.VISIBLE
     }
 }
